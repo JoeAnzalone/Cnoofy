a7489481d941a2c1165049579d1e8795eb62f91e
Joe Anzalone <Joe@Shmit.com>
2013-05-22 20:17:41 -0400

    Install Rails v3.2.13

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cnoofy::Application.config.secret_token = '966063397239d76994a2761bfdd1ebccc74200ed3eb1835bd82ff71b2085121edd5d7a471dea26a8771e1adee9f4077c0df22aea3dca48496308c8ad9acf71e2'
